318c95c411d8350f17e75485b83a88214ed7439a
arnoldzius <arnoldzius@gmail.com>
2022-10-21 10:27:17 +0300

    .

---
+++ constants/index.js
@@ -0,0 +1,15 @@
+const url = require('./url.json');
+const credentials = require('./credentials');
+const roomTypes = require('./roomTypes.json');
+const persons = require('./persons.json');
+const messages = require('./messages.json');
+const envVariables = require('./envVariables.json')
+
+module.exports = {
+    url,
+    credentials,
+    roomTypes,
+    persons,
+    messages,
+    envVariables
+}
